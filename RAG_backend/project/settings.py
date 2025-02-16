7035e682208b82f188c7f123bd12cd7466987934
harsh123-baba <harshitgoyal213@gmail.com>
2025-02-16 19:47:37 +0530

    final commit after aaksh validation

--- RAG_backend/project/settings.py
+++ RAG_backend/project/settings.py
@@ -137,8 +137,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),  
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),  
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
